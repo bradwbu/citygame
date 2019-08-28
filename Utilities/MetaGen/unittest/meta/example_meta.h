38bcd8e54f93aff095c8a28d18ee8353cec1940b
Cattan <ynkelon.gmail.com>
2019-08-27 18:29:52 -0700

    Remove MetaGen

--- Utilities/MetaGen/unittest/meta/example_meta.h
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// META generated file. do not edit!
-// Generated: Fri Nov 07 19:07:34 2008
-// ============================================================
-
-#include "stream.h"
-void /*void*/ voidpppFoo_client( int a, char *b);
