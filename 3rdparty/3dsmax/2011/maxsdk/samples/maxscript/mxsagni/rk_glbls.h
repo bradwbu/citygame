f02c4ae1833fda5a2dea5bfecad927e3d5ffbc1b
Pazaz <pazaz@ourodev.com>
2020-05-09 23:31:06 -0400

    fix: Remove 3rdparty documentation/sample folders

--- 3rdparty/3dsmax/2011/maxsdk/samples/maxscript/mxsagni/rk_glbls.h
+++
@@ -1,2 +0,0 @@
-define_struct_global ( "visible",				"trackbar",				get_trackbar_visible,		set_trackbar_visible);
-define_struct_global ( "filter",				"trackbar",				get_trackbar_filter,		set_trackbar_filter);
